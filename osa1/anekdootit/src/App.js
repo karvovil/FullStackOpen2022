c8a3e1cd851a2a867fd4642658fa3b885d23913c
Ville Karvonen <villekarvonen1@gmail.com>
2022-04-17 13:30:37 +0300

    osa1 completed

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const setSelectedTo = () => { 
+    let rndm = Math.floor( Math.random() * anecdotes.length )
+    return setSelected(rndm)
+  }
+  const handleVote = () => {
+    //const updatedVotes = [...votes.slice(0,selected),votes[selected]+1,...votes.slice(selected+1,)]
+    const updatedVotes = [...votes]
+    updatedVotes[selected] +=1
+    console.log(votes)
+    return setVotes(updatedVotes)
+  }
+  const MostVoted = () => {
+    let indexOfMostVoted = votes.indexOf( Math.max(...votes) )
+    console.log(indexOfMostVoted)
+    return (
+      <p>{anecdotes[indexOfMostVoted]}</p>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={() => setSelectedTo()} text="next anecdote" />
+      <Button handleClick={() => handleVote()} text="vote" />
+      <h1>Most voted anecdote</h1>
+      <MostVoted/>
+    </div>
+  )
+}
+
+export default App
