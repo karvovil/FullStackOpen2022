08ead24672b0e38d09987e79eb2e908f8e46fc8a
Ville Karvonen <villekarvonen1@gmail.com>
2022-04-27 15:44:00 +0300

    2.14 completed

--- osa2/maiden/src/App.js
+++ osa2/maiden/src/App.js
@@ -1,23 +1,24 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const apiKey= "efe70099c6f470afe64760b376f00078"//hide apikey pls
 const Content = ({countries, filter, buttonHandler}) => {
   let filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase() ))
 
   if (filteredCountries.length > 10){
     return <p>Too many countries</p> 
   }else if (filteredCountries.length > 1){
+    console.log(filteredCountries)
     return <Countries countries={filteredCountries} buttonHandler ={buttonHandler}/>
   }else if (filteredCountries.length === 1){
     return <Country country ={filteredCountries[0]}/>
   }
 }
 const Countries = ({ countries, buttonHandler }) => countries.map(country =>
-  <> 
-    <li key={country.name.common}>{country.name.common}</li> 
-    <button onClick={()=>buttonHandler(country.name.common)}>show</button>
-  </>)
+    <li key={country.name.common}>
+      {country.name.common} 
+      <button onClick={()=>buttonHandler(country.name.common)}>show</button>
+    </li> 
+)
 
 const Languages = ({languages}) =>  Object.values(languages).map(value =>  <li key={value}>{value}</li> )
 const Country = ({country}) => {
@@ -36,18 +37,26 @@ const Country = ({country}) => {
   )
 }
 
-const Weather = ({lat, lon})=>{
-  const [w, setW] = useState({})
+const Weather = ({lat, lon}) => {
+  const [weather, setWeather] = useState(null)
   useEffect(() => {
     axios
       .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${"efe70099c6f470afe64760b376f00078"}`)
       .then(response => {
-        setW(response.data)
+        console.log("weather fullfilled");
+        console.log(response);
+        setWeather(response.data)
       })
   }, [])
-  console.log("effect", w)
-  return <p>weather: {JSON.stringify(w.main)}</p>
+    const icon = weather ? weather.weather[0].icon : null
+  return(
+    <div>
+      <p>Temperature: {weather ? weather.main.temp : null}</p>
+      <img  src={`http://openweathermap.org/img/w/${icon}.png`} alt="icon"/>
+      <p>Wind: {weather ? weather.wind.speed : null}</p>
 
+    </div>
+    )
 }
 const Filter = ({filter, handleFilterChange}) => {
   return(
@@ -71,7 +80,7 @@ const App = () => {
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
         console.log('promise fulfilled')
-        console.log(response.data)
+
         setCountries(response.data)
       })
   }, [])
