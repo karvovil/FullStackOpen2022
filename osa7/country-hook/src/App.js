43eee1d22e9e99fd1e15c58fdefd685d04097692
Ville Karvonen <villekarvonen1@gmail.com>
2022-06-13 14:41:08 +0300

    7.7 working

--- osa7/country-hook/src/App.js
+++ osa7/country-hook/src/App.js
@@ -18,7 +18,17 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+      axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then((response) => {
+        console.log(response)
+        setCountry(response.data[0])
+      }).catch(erro =>{
+        console.log(erro)
+        setCountry(null)
+      })
+  },[name])
 
   return country
 }
