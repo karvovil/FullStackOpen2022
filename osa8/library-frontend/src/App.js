e8982087b097a103156c9bf2e559159a895d9719
Ville Karvonen <villekarvonen1@gmail.com>
2022-07-18 12:53:11 +0300

    8.9 allBooks query working

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -13,16 +13,26 @@ const ALL_AUTHORS = gql`
     }
   }
 `
+const ALL_BOOKS = gql`
+  query {
+    allBooks  {
+      title,
+      author,
+      published
+    }
+  }
+`
 
 const App = () => {
   const [page, setPage] = useState('authors')
 
-  const result = useQuery( ALL_AUTHORS )
+  const authorResult = useQuery( ALL_AUTHORS )
+  const bookResult = useQuery( ALL_BOOKS )
 
-  if (result.loading)  {
+  if (authorResult.loading || bookResult.loading)  {
     return <div>loading...</div>
   }
-  console.log(result.data)
+  console.log(bookResult.data)
   return (
     <div>
       <div>
@@ -31,9 +41,9 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} authors={result.data.allAuthors} />
+      <Authors show={page === 'authors'} authors={authorResult.data.allAuthors} />
 
-      <Books show={page === 'books'} />
+      <Books show={page === 'books'} books={bookResult.data.allBooks} />
 
       <NewBook show={page === 'add'} />
     </div>
