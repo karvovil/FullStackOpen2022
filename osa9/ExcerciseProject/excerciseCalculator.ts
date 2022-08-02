c3336ee165133ff2120fb56758045b6cda1aed0c
Ville Karvonen <villekarvonen1@gmail.com>
2022-08-02 13:05:23 +0300

    9.6 eslint inittialized

--- osa9/ExcerciseProject/excerciseCalculator.ts
+++ osa9/ExcerciseProject/excerciseCalculator.ts
@@ -1,60 +1,60 @@
-interface ExcerciseValues {
+export interface ExcerciseValues {
         periodLength: number,
         trainingDays: number,
         success: boolean,
         rating: number,
         ratingDescription: string,
         target: number,
-        average: number 
+        average: number
   }
-interface ExcerciseParameters {
-    hours: Array<number> 
+export interface ExcerciseParameters {
+    hours: Array<number>,
     target: number,
   }
-  const parseValues = (args: Array<string>): ExcerciseParameters => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (isNaN(Number(args[2]))) throw new Error('Provided target value was not number!');
-    const target: number = Number(args[2]);
-    const hourStrings: Array<string> = args.splice(3)
-    const hours: Array<number> = hourStrings.map(n=>Number(n));
-    if (hours.includes(NaN)) throw new Error('Invalid arguments');
-    return {
-        hours,
-        target
-    };
-  }
+export const parseValues = (args: Array<string>): ExcerciseParameters => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (isNaN(Number(args[2]))) throw new Error('Provided target value was not number!');
+  const target = Number(args[2]);
+  const hourStrings: Array<string> = args.splice(3);
+  const hours: Array<number> = hourStrings.map(n => Number(n));
+  if (hours.includes(NaN)) throw new Error('Invalid arguments');
+  return {
+    hours,
+    target
+  };
+};
 
-const calculateExcercises = ( {hours, target}: ExcerciseParameters ): ExcerciseValues => {
+export const calculateExcercises = ( { hours, target }: ExcerciseParameters ): ExcerciseValues => {
 
-    const periodLength = hours.length;
-    const trainingDays = hours.filter(d => d>0).length;
-    const average = hours.reduce((a,b)=> a+b, 0 )/periodLength;
-    const success = average >= target;
-    const rating = 
+  const periodLength = hours.length;
+  const trainingDays = hours.filter(d => d>0).length;
+  const average = hours.reduce((a,b) => a+b, 0 )/periodLength;
+  const success = average >= target;
+  const rating =
         average < target-1 ? 1 :
-        average > target+1 ? 3 : 
-        2;
-    const ratingDescription = 
+          average > target+1 ? 3 :
+            2;
+  const ratingDescription =
     rating === 1 ? 'too bad' :
-    rating === 2 ? 'not too bad but could be better' : 
-    'better';
+      rating === 2 ? 'not too bad but could be better' :
+        'better';
 
-    return { 
-        periodLength,
-        trainingDays,
-        success,
-        rating,
-        ratingDescription,
-        target,
-        average 
-    }
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  };
+};
+try {
+  console.log(calculateExcercises(parseValues(process.argv)));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
   }
-  try {
-    console.log(calculateExcercises(parseValues(process.argv)));
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.'
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    console.log(errorMessage);
-  }
+  console.log(errorMessage);
+}
